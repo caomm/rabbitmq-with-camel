e782fd85c31f9b2df1d0a0f80efe4188b223e975
Ivan Mircic <ivan.mircic@gmail.com>
2016-06-29 01:31:03 +0300

    - Add properties file use for spring route - Add usage of template variable, already defined in CamelTestSupport class

--- src/main/java/com/mircic/route/RabbitMQRoute.java
+++ src/main/java/com/mircic/route/RabbitMQRoute.java
@@ -11,11 +11,11 @@ import org.springframework.stereotype.Component;
 public class RabbitMQRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from("rabbitmq://localhost:5672/inputQueue?queue=inputQueue")
+        from("rabbitmq://{{rabbitmq.host}}:5672/inputQueue?queue=inputQueue")
                 .process(exchange -> {
                     exchange.getIn().setHeader("rabbitmq.ROUTING_KEY", "exit");
                     exchange.getIn().setHeader("rabbitmq.EXCHANGE_NAME", "exitQueue");
                 }).to("log:com.mircic.route.RabbitMQRoute?level=INFO")
-                .to("rabbitmq://localhost:5672/exitQueue?queue=exitQueue&routingKey=exit");
+                .to("rabbitmq://{{rabbitmq.host}}:5672/exitQueue?queue=exitQueue&routingKey=exit");
     }
 }
--- src/test/java/com/mircic/route/RabbitMQRouteTest.java
+++ src/test/java/com/mircic/route/RabbitMQRouteTest.java
@@ -1,7 +1,5 @@
 package com.mircic.route;
 
-import org.apache.camel.Produce;
-import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.NotifyBuilder;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.commons.io.IOUtils;
@@ -14,15 +12,12 @@ import java.io.IOException;
  */
 public class RabbitMQRouteTest extends CamelTestSupport {
 
-    @Produce(uri = "rabbitmq://localhost:5672/inputQueue?queue=inputQueue")
-    protected ProducerTemplate producer;
-
     @Test
     public void testNormalClient() throws IOException{
         NotifyBuilder notify = new NotifyBuilder(context).whenDone(1).create();
 
         String message1Sent = IOUtils.toString(this.getClass().getResourceAsStream("/test-data/message1.xml"));
-        producer.sendBody(message1Sent);
+        template().sendBody("rabbitmq://localhost:5672/inputQueue?queue=inputQueue", message1Sent);
 
         notify.from("rabbitmq://localhost:5672/exitQueue?queue=exitQueue&routingKey=exit").equals(message1Sent);
     }
