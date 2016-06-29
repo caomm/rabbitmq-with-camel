aaa6e3f7016aa58fa77d3b98859af342e0f3e537
Ivan Mircic <ivan.mircic@gmail.com>
2016-06-29 03:44:36 +0300

    - Change logic a bit to highlight what I actually try to show here(rabbitmq routing with help of camel) - Move rest service in same class(no need to complicate things)

--- src/main/java/com/mircic/route/RabbitMQRoute.java
+++ src/main/java/com/mircic/route/RabbitMQRoute.java
@@ -2,20 +2,39 @@ package com.mircic.route;
 
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
- * Created by Ivan on 03-Jan-16.
+ * This route represent example how RabbitMQ intyernal routing actually work.
+ *
+ * Here are two routes(separate ones)
+ *
+ * 1. Simple Rest server which allow us to post to input queue
+ * 2. RabbitMQ one which actually read from exit queue
+ *
+ * Roting is done by using routing_key and routing_exchange_name values
+ * (in that way rabbitmq recognize to qhihc queue message eventually go)
  */
 @Component
 public class RabbitMQRoute extends RouteBuilder {
+    @Value("${rest.port}")
+    private Integer restPort;
+
     @Override
     public void configure() throws Exception {
-        from("rabbitmq://{{rabbitmq.host}}:5672/inputQueue?queue=inputQueue")
+        restConfiguration().component("jetty")
+                .port(restPort);
+
+        from("rest:post:/rest/postMessageToInputQueue")
                 .process(exchange -> {
                     exchange.getIn().setHeader("rabbitmq.ROUTING_KEY", "exit");
                     exchange.getIn().setHeader("rabbitmq.EXCHANGE_NAME", "exitQueue");
-                }).to("log:com.mircic.route.RabbitMQRoute?level=INFO")
-                .to("rabbitmq://{{rabbitmq.host}}:5672/exitQueue?queue=exitQueue&routingKey=exit");
+                })
+                .inOnly("rabbitmq://{{rabbitmq.host}}:5672/inputQueue?queue=inputQueue");
+
+
+        from("rabbitmq://{{rabbitmq.host}}:5672/exitQueue?queue=exitQueue&routingKey=exit")
+                .to("log:com.mircic.route.RabbitMQRoute?level=INFO");
     }
 }
